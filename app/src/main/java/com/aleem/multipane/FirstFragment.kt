88558721bff9be6b672e1ea48e4036fe22361122
Aleem Awan <aleemawan366@outlook.com>
2022-06-02 18:05:20 +0500

    adds click item click listener in recycler view

--- app/src/main/java/com/aleem/multipane/FirstFragment.kt
+++ app/src/main/java/com/aleem/multipane/FirstFragment.kt
@@ -1,10 +1,14 @@
 package com.aleem.multipane
 
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -44,6 +48,15 @@ class FirstFragment : Fragment() {
 
         binding.rvGrid.layoutManager = GridLayoutManager(context,4,)
         binding.rvGrid.adapter = itemAdapter
+
+        itemAdapter.setOnItemClickListener(object :OnItemClickListener{
+            override fun onItemClick(position: Int,view: View) {
+                val tv = view.findViewById(R.id.tvItemName) as TextView
+                tv.setTextColor(Color.parseColor("#fd00ab"))
+                tv.setBackgroundColor(Color.parseColor("#1400fd"))
+            }
+
+        })
 
     }
 
--- app/src/main/java/com/aleem/multipane/ItemAdapter.kt
+++ app/src/main/java/com/aleem/multipane/ItemAdapter.kt
@@ -1,15 +1,27 @@
 package com.aleem.multipane
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class ItemViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
+class ItemViewHolder(itemView: View,listener: OnItemClickListener):RecyclerView.ViewHolder(itemView){
     val tvItem = itemView.findViewById<TextView>(R.id.tvItemName)
+    init {
+        itemView.setOnClickListener {
+            listener.onItemClick(adapterPosition,it)
+        }
+    }
 }
+
+interface OnItemClickListener{
+    fun onItemClick(position: Int,view:View)
+}
+
 class ItemAdapter:RecyclerView.Adapter<ItemViewHolder>() {
+    private lateinit var listener: OnItemClickListener
     val itemList = listOf(
         ItemName("Chicken"),
         ItemName("Biryani"),
@@ -26,11 +38,17 @@ class ItemAdapter:RecyclerView.Adapter<ItemViewHolder>() {
         ItemName("Mutton Tika"),
         ItemName("Beef Pulao"),
     )
+
+    fun setOnItemClickListener(mListener: OnItemClickListener){
+        Log.i("MLait","here")
+        listener = mListener
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
        val context = parent.context
         val inflater = LayoutInflater.from(context)
         val view = inflater.inflate(R.layout.item_layout,parent,false)
-        return ItemViewHolder(view)
+        return ItemViewHolder(view,listener)
     }
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
